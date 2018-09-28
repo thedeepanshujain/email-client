64d8015d7d044540866de2f4765401a97669395a
Deepanshu Jain <deepanshujain@Deepanshus-MacBook-Air.local>
2018-09-29 03:09:35 +0530

    all new messages to db

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,23 +2,41 @@ require 'mail'
 
 module MessagesHelper
 
-	def create_mail(email_from, email_to, subject, message_text, file_path, references, reply_to)
+	def add_to_db(messages_gmail, status)
+
+		messages_gmail.each do |message_gmail|
+			message_id = message_gmail.id
+			message_db = Message.find_by(message_id: message_id)
+			if message_db.nil?
+				message_db = Message.new
+				message_db = gmail_to_db(message, status)
+			end
+
+			if message_db.save
+				"Saved to db : "+message_id.to_s
+			else
+				raise 'Unable to save to db. Please check.'
+			end
+		end
+
+	end
+
+	def create_mail(email_from, email_to, subject, message_text, references, reply_to, file_path = nil)
 		
-		unless file_path.nil? 
-			file_name = File.basename(file_path)
-		end
-
-		mail = Mail.new do
-			from     email_from
-			to       email_to
-			subject  subject
-			body     message_text
-		end
-
-		mail.add_file(:filename => file_name, :content => File.read(file_path))
+		mail = Mail.new 
+		mail.from = email_from
+		mail.to = email_to
+		mail.subject = subject
+		mail.body = message_text
 		mail.references= references
 		mail.in_reply_to= reply_to
 
+		if !file_path.nil? && !file_path.empty?
+			file_name = File.basename(file_path)
+			mail.add_file(:filename => file_name, :content => File.read(file_path))
+		end
+
 		return mail
 	end
+
 end
